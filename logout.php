9fafbfceb5a3f13afeaf05dcab8cf7867339f97c
Dewwerd <Dewwerd@tut.by>
2021-12-04 14:35:26 +0300

    second commit

--- logout.php
+++ logout.php
@@ -6,7 +6,7 @@
 	include 'signUpVk.php';
 	$_SESSION['vk'] = "";
 	$_SESSION['vk_in'] == 'false';
-	//logOut();
+	logOut();
 	header("location: / ");
 	//echo $link = '<p><a href="/">На главную</a></p>';
 ?>
